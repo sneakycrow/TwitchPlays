32682ec62638dbe8326feb55463883187abf82fd
sneakycrow <zach@sneakycrow.dev>
2023-04-06 18:15:44 -0700

    add frontend receiver for messages

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import SVG from 'react-inlinesvg';
 import './App.css'
+import {listen} from '@tauri-apps/api/event'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
+    const [message, setMessage] = useState("");
 
-  return (
-      <SVG
-        src="/controller.svg"
-        width={1200}
-        height="auto"
-        title="React"
-        className="controller"
-      />
-  )
+    const fn = async () => {
+        await listen('send_message', (event) => {
+            // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
+            // event.payload is the payload object
+            setMessage(event.payload as string);
+        })
+    };
+
+    fn().catch(e => console.error(e));
+    return (
+        <div className="wrapper">
+            <SVG
+                src="/controller.svg"
+                width={1200}
+                height="auto"
+                title="React"
+                className="controller"
+            />
+            <p>{message}</p>
+        </div>
+    )
 }
 
 export default App
