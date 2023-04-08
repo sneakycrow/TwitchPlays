adfb0f10459dc7425e5831a0b5d6eb854aa085ef
ctrlsquid <106559028+ctrlsquid@users.noreply.github.com>
2023-04-08 13:22:06 -0700

    Merge pull request #29
    * move components to src folder

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,22 +1,17 @@
-import {useState} from 'react'
-import {listen} from '@tauri-apps/api/event'
+import React from "react";
+import Footer from "./components/Footer";
 
 function App() {
-    const [message, setMessage] = useState("example message");
-
-    const fn = async () => {
-        await listen('send_message', (event) => {
-            // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
-            // event.payload is the payload object
-            setMessage(event.payload as string);
-        })
-    };
-
-    fn().catch(e => console.error(e));
     return (
-        <div className="flex bg-white justify-center align-center items-center h-screen">
-            <h1 className="text-7xl text-black font-sans text-center">{message}</h1>
-        </div>
+        <>
+            <div className="flex bg-white justify-center align-center items-center min-h-[75vh]">
+                main area
+            </div>
+            <h1 className="text-7xl font-bold underline">
+                Hello world!
+            </h1>
+            <Footer/>
+        </>
     )
 }
 
---
+++ ui/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import Message from "./Message";
+
+const Footer = () => {
+    return (
+        <footer className="border-4">
+            <Message/>
+        </footer>
+    )
+}
+
+export default Footer
