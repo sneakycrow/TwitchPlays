c0c3c2ea9db90f29df94f79e3c00b6c8e77db5ea
sneakycrow <zach@sneakycrow.dev>
2023-05-06 12:14:24 -0700

    convert main fn to use dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
-use tracing::event;
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use tracing::{event, Level};
 use twitch_irc::{ClientConfig, SecureTCPTransport, TwitchIRCClient};
 use twitch_irc::login::StaticLoginCredentials;
 use twitch_irc::message::ServerMessage::Privmsg;
 
 fn main() {
-    println!("Hello, world!");
+    dioxus_desktop::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+        },
+        Button {}
+    })
+}
+
+fn Button(cx: Scope) -> Element {
+    cx.render(rsx! {
+        button {
+            onclick: move |event| {
+                println!("Clicked! Event: {event:?}")
+            },
+            "Click me!"
+        }
+    })
 }
 
 async fn connect_to_chat() {
@@ -19,15 +42,14 @@ async fn connect_to_chat() {
             match message {
                 Privmsg(msg) => {
                     event!(
-                                    target: "chat_messages",
-                                    Level::INFO,
-                                    channel = msg.channel_login,
-                                    sender = msg.sender.name,
-                                    text = msg.message_text,
-                                    server_timestamp = msg.server_timestamp.to_string(),
-                                    id = msg.message_id,
-                                );
-                    send_message(msg.message_text, &app_handle);
+                        target: "chat_messages",
+                        Level::INFO,
+                        channel = msg.channel_login,
+                        sender = msg.sender.name,
+                        text = msg.message_text,
+                        server_timestamp = msg.server_timestamp.to_string(),
+                        id = msg.message_id,
+                    );
                 }
                 _ => {}
             }
