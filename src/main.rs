6e409b9bc40961db91a739c1b106e24af9b0b1bb
ctrlsquid <106559028+ctrlsquid@users.noreply.github.com>
2023-05-06 12:15:18 -0700

    Replace Tauri with Dioxus (#32)
    * remove all tauri deps
    
    * add dioxus deps
    
    * convert main fn to use dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,62 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![allow(non_snake_case)]
 
-
-use tauri::Manager;
-use tracing::{event, info, Level};
-use tracing_subscriber::EnvFilter;
+use dioxus::prelude::*;
+use tracing::{event, Level};
 use twitch_irc::{ClientConfig, SecureTCPTransport, TwitchIRCClient};
 use twitch_irc::login::StaticLoginCredentials;
 use twitch_irc::message::ServerMessage::Privmsg;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-// A function that sends a message from Rust to JavaScript via a Tauri Event
-fn send_message<R: tauri::Runtime>(message: String, manager: &impl Manager<R>) {
-    info!(?message, "send_message");
-    manager
-        .emit_all("send_message", message)
-        .unwrap();
+fn main() {
+    dioxus_desktop::launch(App);
 }
 
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+        },
+        Button {}
+    })
+}
 
-fn main() {
-    tracing_subscriber::fmt()
-        .with_env_filter(
-            EnvFilter::from_default_env()
-                .add_directive("chat_messages=info".parse().unwrap())
-                .add_directive("twitch_irc=off".parse().unwrap()),
-        )
-        .with_target(true)
-        .with_timer(tracing_subscriber::fmt::time::UtcTime::rfc_3339()).init();
-    tauri::Builder::default()
-        .setup(|app| {
-            let app_handle = app.handle();
-            tauri::async_runtime::spawn(async move {
-                let config = ClientConfig::default();
+fn Button(cx: Scope) -> Element {
+    cx.render(rsx! {
+        button {
+            onclick: move |event| {
+                println!("Clicked! Event: {event:?}")
+            },
+            "Click me!"
+        }
+    })
+}
 
-                let (mut incoming_messages, client) =
-                    TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
-                // Create an instance of Analyzer for executing thresholds and triggers
-                // Spawn an async instance that catches the messages
-                let join_handle = tokio::spawn(async move {
-                    while let Some(message) = incoming_messages.recv().await {
-                        match message {
-                            Privmsg(msg) => {
-                                event!(
-                                    target: "chat_messages",
-                                    Level::INFO,
-                                    channel = msg.channel_login,
-                                    sender = msg.sender.name,
-                                    text = msg.message_text,
-                                    server_timestamp = msg.server_timestamp.to_string(),
-                                    id = msg.message_id,
-                                );
-                                send_message(msg.message_text, &app_handle);
-                            }
-                            _ => {}
-                        }
-                    }
-                });
+async fn connect_to_chat() {
+    let config = ClientConfig::default();
 
-                client.join(String::from("zackrawrr")).unwrap();
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+    // Create an instance of Analyzer for executing thresholds and triggers
+    // Spawn an async instance that catches the messages
+    let join_handle = tokio::spawn(async move {
+        while let Some(message) = incoming_messages.recv().await {
+            match message {
+                Privmsg(msg) => {
+                    event!(
+                        target: "chat_messages",
+                        Level::INFO,
+                        channel = msg.channel_login,
+                        sender = msg.sender.name,
+                        text = msg.message_text,
+                        server_timestamp = msg.server_timestamp.to_string(),
+                        id = msg.message_id,
+                    );
+                }
+                _ => {}
+            }
+        }
+    });
 
-                join_handle.await.unwrap();
-            });
+    client.join(String::from("zackrawrr")).unwrap();
 
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    join_handle.await.unwrap();
 }
