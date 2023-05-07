6b2ae269523ff2111c44aecd37b44f2b0b6e9ba8
sneakycrow <zach@sneakycrow.dev>
2023-05-07 16:34:43 -0700

    move twitch chat connection logic to app component

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,6 +1,40 @@
 use dioxus::prelude::*;
+use tracing::{event, Level};
+use twitch_irc::{ClientConfig, SecureTCPTransport, TwitchIRCClient};
+use twitch_irc::login::StaticLoginCredentials;
+use twitch_irc::message::ServerMessage::Privmsg;
 
 pub(crate) fn App(cx: Scope) -> Element {
+    let chat: &Coroutine<()> = use_coroutine(cx, |rx| async move {
+        let config = ClientConfig::default();
+
+        let (mut incoming_messages, client) =
+            TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+        // Create an instance of Analyzer for executing thresholds and triggers
+        // Spawn an async instance that catches the messages
+        let join_handle = tokio::spawn(async move {
+            while let Some(message) = incoming_messages.recv().await {
+                match message {
+                    Privmsg(msg) => {
+                        event!(
+                        target: "chat_messages",
+                        Level::INFO,
+                        channel = msg.channel_login,
+                        sender = msg.sender.name,
+                        text = msg.message_text,
+                        server_timestamp = msg.server_timestamp.to_string(),
+                        id = msg.message_id,
+                    );
+                    }
+                    _ => {}
+                }
+            }
+        });
+
+        client.join(String::from("zackrawrr")).unwrap();
+
+        join_handle.await.unwrap();
+    });
     cx.render(rsx! {
         div {
             "Hello, world!"
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,20 @@
 #![allow(non_snake_case)]
 
-use tracing::{event, Level};
-use twitch_irc::{ClientConfig, SecureTCPTransport, TwitchIRCClient};
-use twitch_irc::login::StaticLoginCredentials;
-use twitch_irc::message::ServerMessage::Privmsg;
+
+use tracing_subscriber::EnvFilter;
 
 use crate::components::App;
 
 mod components;
 
 fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::from_default_env()
+                .add_directive("chat_messages=info".parse().unwrap())
+                .add_directive("twitch_irc=off".parse().unwrap()),
+        )
+        .with_target(true)
+        .with_timer(tracing_subscriber::fmt::time::UtcTime::rfc_3339()).init();
     dioxus_desktop::launch(App);
-}
-
-async fn connect_to_chat() {
-    let config = ClientConfig::default();
-
-    let (mut incoming_messages, client) =
-        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
-    // Create an instance of Analyzer for executing thresholds and triggers
-    // Spawn an async instance that catches the messages
-    let join_handle = tokio::spawn(async move {
-        while let Some(message) = incoming_messages.recv().await {
-            match message {
-                Privmsg(msg) => {
-                    event!(
-                        target: "chat_messages",
-                        Level::INFO,
-                        channel = msg.channel_login,
-                        sender = msg.sender.name,
-                        text = msg.message_text,
-                        server_timestamp = msg.server_timestamp.to_string(),
-                        id = msg.message_id,
-                    );
-                }
-                _ => {}
-            }
-        }
-    });
-
-    client.join(String::from("zackrawrr")).unwrap();
-
-    join_handle.await.unwrap();
 }
