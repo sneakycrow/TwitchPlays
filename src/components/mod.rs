65f77fbc39706a0c12c3599065dbb8c653fbde3a
sneakycrow <zach@sneakycrow.dev>
2023-05-08 11:22:12 -0700

    move connection out of secondary async spawner

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -5,39 +5,40 @@ use twitch_irc::login::StaticLoginCredentials;
 use twitch_irc::message::ServerMessage::Privmsg;
 
 pub(crate) fn App(cx: Scope) -> Element {
-    let chat: &Coroutine<()> = use_coroutine(cx, |rx| async move {
-        let config = ClientConfig::default();
+    let config = ClientConfig::default();
 
-        let (mut incoming_messages, client) =
-            TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+    let chat: &Coroutine<()> = use_coroutine(cx, |rx| async move {
         // Create an instance of Analyzer for executing thresholds and triggers
         // Spawn an async instance that catches the messages
-        let join_handle = tokio::spawn(async move {
-            while let Some(message) = incoming_messages.recv().await {
-                match message {
-                    Privmsg(msg) => {
-                        event!(
-                        target: "chat_messages",
-                        Level::INFO,
-                        channel = msg.channel_login,
-                        sender = msg.sender.name,
-                        text = msg.message_text,
-                        server_timestamp = msg.server_timestamp.to_string(),
-                        id = msg.message_id,
-                    );
-                    }
-                    _ => {}
-                }
-            }
-        });
-
         client.join(String::from("zackrawrr")).unwrap();
-
-        join_handle.await.unwrap();
+        while let Some(message) = incoming_messages.recv().await {
+            match message {
+                Privmsg(msg) => {
+                    event!(
+                            target: "chat_messages",
+                            Level::INFO,
+                            channel = msg.channel_login,
+                            sender = msg.sender.name,
+                            text = msg.message_text,
+                            server_timestamp = msg.server_timestamp.to_string(),
+                            id = msg.message_id,
+                        );
+                    let message = format!(
+                        "[{}] {}: {}",
+                        msg.server_timestamp.format("%H:%M:%S"),
+                        msg.sender.name,
+                        msg.message_text
+                    );
+                }
+                _ => {}
+            }
+        }
     });
     cx.render(rsx! {
         div {
-            "Hello, world!"
+            "test"
         },
         Button {}
     })
